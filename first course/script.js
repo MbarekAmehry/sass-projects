eacaa9381162cc873d91f791a0602188d7bbb30a
Amehry Mbarek <mbarek.amehry@gmail.com>
2020-06-15 18:38:27 +0100

    first commit

---
+++ first course/script.js
@@ -0,0 +1,8 @@
+const showcase = document.querySelector(".showcase");
+const checkbox = document.querySelector(".checkbox");
+
+checkbox.addEventListener("click", e => {
+  showcase.classList.toggle("dark");
+  //   if (checkbox.checked) {
+  //   }
+});
